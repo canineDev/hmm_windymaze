69bcb81fe6a0c9250f58b4791015243a9c69a99f
CryptoCANINE <clodcena@gmail.com>
2026-07-26 13:28:41 -0400

    added definitions and maze

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <vector>
 
+#define ROWS 5
+#define COLS 6
+#define START_ROW
+#define START_COL
+#define GOAL_ROW
+#define GOAL_COL
+
 using namespace std;    // bc im lazy
+
+int maze[ROWS][COLS] = {
+    {0, 0, 0, 0, 0, 0},
+    {0, 1, 1, 1, 1, 0},
+    {0, 1, 0, 0, 1, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+};
 
 int main(){
     cout << "can u hear me....";
